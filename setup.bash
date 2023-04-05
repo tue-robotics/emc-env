7102f69821ebe9d70c64b6c8f9412fced72acdfe
PetervDooren <19806646+PetervDooren@users.noreply.github.com>
2023-04-05 11:43:35 +0200

    mrc-open door

--- setup.bash
+++ setup.bash
@@ -66,8 +66,7 @@ function emc-update
 # --------------------------------------------------------------------------------
 
 alias mrc-teleop='rosrun emc_system teleop.py'
-alias hero-open-door='rostopic pub --once /hero/open_door std_msgs/Empty "{}"'
-alias pyro-open-door='rostopic pub --once /pyro/open_door std_msgs/Empty "{}"'
+alias mrc-open-door='rostopic pub --once /hero/open_door std_msgs/Empty "{}"'
 
 alias mrc-update=emc-update
 
